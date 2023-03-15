cbd70094b24719b19694d4aed5f26846dae6caf8
Microd18 <Microd@inbox.ru>
2023-03-16 00:07:25 +0500

    Homework done!

--- src/main/java/com/example/calculator/calculator/CalculatorController.java
+++ src/main/java/com/example/calculator/calculator/CalculatorController.java
@@ -1,5 +1,41 @@
 package com.example.calculator.calculator;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/calculator")
 public class CalculatorController {
+    private final CalculatorService service;
+
+    public CalculatorController(CalculatorService service){
+        this.service = service;
+    }
+
+    @GetMapping
+    public String helloCalculator() {
+        return service.helloCalculator();
+    }
+
+    @GetMapping("/plus")
+    public String plus(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+        return service.plus(num1, num2);
+    }
+
+    @GetMapping("/minus")
+    public String minus(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+        return service.minus(num1, num2);
+    }
+
+    @GetMapping("/multiply")
+    public String multiply(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+        return service.multiply(num1, num2);
+    }
+
+    @GetMapping("/divide")
+    public String divide(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+        return service.divide(num1, num2);
+    }
 }
